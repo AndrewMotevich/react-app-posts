ce0258e6517d6296da574431723a91ef093b8385
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-03-15 14:42:50 +0300

    Updates

--- src/components/utils/pages.js
+++
@@ -1,5 +0,0 @@
-const getPageCount = (totalPages, limit) => {
-  return Math.ceil(totalPages / limit);
-};
-
-export default getPageCount;
--- src/pages/Page404.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Page404 = () => {
-  return <div style={{ color: "blue", fontSize: "50px" }}>Page404</div>;
-};
-
-export default Page404;
